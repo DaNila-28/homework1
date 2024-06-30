285ea5871c158b76194e6b3ade2e88b523e412d8
DaNila-28 <d12t28@mail.ru>
2024-06-30 22:06:03 +0300

    Добавлена длинна строки

--- homework1.py
+++ homework1.py
@@ -1,6 +1,10 @@
 example = "Эксклюзив"
+len_example = len(example)
+
 print(example[0])
 print(example[-1])
 print(example[4::])
 print(example[::-1])
 print(example[1:9:2])
+
+print(f"Длинна строки равна:{len_example}")
